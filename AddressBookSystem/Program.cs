3229fd473270aa08ffb36ba66ef003d139b86340
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-26 23:45:56 +0530

    Added EditContactUC3 in program

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -5,13 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("===============================\nWelcome To Address Book System\n===============================");
-            Console.WriteLine("\n1. Add New Contact.\n");
+            Console.WriteLine("\n1. Create Contact.\n2. Add Contact and Display\n3. Edit Existing Contact\n");
             int option = Convert.ToInt32(Console.ReadLine());
+            AddressBook addressBook = new AddressBook();
             switch (option)
             {
-                //UC1 - Add a New Contact
+                //UC1 - Create Contact
                 case 1:
-                    AddressBook.CreateContacts();
+                    addressBook.CreateContacts();
+                    break;
+                //UC2 - Add Contact and Display
+                case 2:
+                    addressBook.DisplayContact();
+                    break;
+                //UC3 - Exit Existing Contact
+                case 3:
+                    addressBook.EditContact();
                     break;
                 default:
                     Console.WriteLine("Choose from given option only!\n");
