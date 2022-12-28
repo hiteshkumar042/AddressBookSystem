509e546bb9822de2e511750e04264ca2ff556d57
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-28 15:13:31 +0530

    Revert "Multiple Contact adding ability Added"
    This reverts commit a76925a993db07d4db30388bedaa4b6d21ddd445.

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -15,13 +15,7 @@
                 {
                     //UC1 - Create Contact
                     case 1:
-                        //UC5 - Add multiple Contact
-                        Console.WriteLine("How many Contact want to add ?");
-                        int howMany=Convert.ToInt32(Console.ReadLine());
-                        for (int i=1; i <= howMany; i++)
-                        {
-                            addressBook.CreateContacts();
-                        }
+                        addressBook.CreateContacts();
                         break;
                     //UC2 - Add Contact and Display
                     case 2:
